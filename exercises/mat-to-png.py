e7a88ffe67635c0aff826e810cbb5622f8bda758
FabianSut <Fabian-Sutter@edu.gbsl.com>
2026-09-04 16:27:20 +0200

    begonnen mit mat-to-png

--- exercises/mat-to-png.py
+++ exercises/mat-to-png.py
@@ -1,5 +1,9 @@
 import png # Paket png importieren
 
+def write(temp):
+    for i in range(faktor):
+        big_smiley.append(temp)
+
 faktor = 2
 
 smiley = [
@@ -11,4 +15,16 @@ smiley = [
     [000, 000, 000, 000, 000, 000]
 ]
 # Erzeuge ein Graustufen-Bild (0=Schwarz, 255=Weiss)
+
+big_smiley = []
+
+temp = []
+
+for zeile in smiley:
+    for pixel in zeile:
+        for i in range(faktor):
+            temp.append(smiley[zeile][pixel])
+        write(temp)
+
+
 png.from_array(smiley, 'L').save('small_smiley.png')
