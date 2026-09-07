695a49bddcc3ce281e94464ca94751324d87d748
FabianSut <Fabian-Sutter@edu.gbsl.com>
2026-09-07 22:21:51 +0200

    Starte scalable_v2

---
+++ exercises/mat-to-png_scalable_v2.py
@@ -0,0 +1,47 @@
+import png # Paket png importieren
+
+def write(temp):
+    for i in range(faktor):
+        big_smiley.insert((len(big_smiley)),temp)
+
+faktor = 10
+
+smiley = [
+    [000, 000, 000, 000, 000, 000],
+    [000, 255, 000, 000, 255, 000],
+    [000, 000, 000, 000, 000, 000],
+    [000, 255, 000, 000, 255, 000],
+    [000, 000, 255, 255, 000, 000],
+    [000, 000, 000, 000, 000, 000]
+]
+# Erzeuge ein Graustufen-Bild (0=Schwarz, 255=Weiss)
+
+big_smiley = []
+
+temp = []
+
+z = 0
+p = 0
+
+for zeile in smiley:
+    for pixel in zeile:
+        for i in range(faktor):
+            temp.append(smiley[z][p])
+        p = p + 1
+    write(temp)
+    temp = []
+    p = 0
+    z = z + 1
+z = 0
+
+# print(big_smiley)
+
+
+# v2
+
+big_smiley_v2 = []
+
+
+
+
+png.from_array(big_smiley, 'L').save('big_smiley_v2.png')
