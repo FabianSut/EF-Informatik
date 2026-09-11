d6835b4d0ab5c6aec1831f029385590b01c837db
FabianSut <Fabian-Sutter@edu.gbsl.com>
2026-09-11 15:53:29 +0200

    PRIMZAHLEN FUNKTIONIERT

--- exercises/primzahlen.py
+++ exercises/primzahlen.py
@@ -4,7 +4,7 @@ primzahlen = list(range(2, 101))
 
 for n in hundert:
     for m in primzahlen:
-        if m % n != 0:
+        if m % n == 0 and m != n:
             primzahlen.pop(primzahlen.index(m))
 
 print(primzahlen)
